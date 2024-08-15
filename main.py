935686507a7e2962225f9156e452360bacabb0db
Adocal5353 <calebadogli53@gmail.com>
2024-08-15 22:01:13 +0000

    change app name to appli

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from fastapi.responses import RedirectResponse
 from app.routes import parentController, enfantController, adminController, tempsEcranController, videoController, saisonController, serieController, categorieController, categorieVideoController 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-app = FastAPI()
+appli = FastAPI()
 origins = [
     "http://localhost:3000",  
     "http://localhost:8000",  
    
 ]
 
-app.add_middleware(
+appli.add_middleware(
     CORSMiddleware,
      allow_origins=["*"],  # Permettre ces origines
     allow_credentials=True,
@@ -22,15 +22,15 @@ app.add_middleware(
 )
 
 
-app.include_router(enfantController.router, prefix="/enfant", tags=["enfant"])
-app.include_router(parentController.router, prefix="/parent", tags=["parent"])
-app.include_router(adminController.router, prefix="/admin", tags=["admin"])
-app.include_router(tempsEcranController.router, prefix="/tempsEcran", tags=["tempsEcran"])
-app.include_router(videoController.router, prefix="/video", tags=["video"])
-app.include_router(saisonController.router, prefix="/saison", tags=["saison"])
-app.include_router(serieController.router, prefix="/serie", tags=["serie"])
-app.include_router(categorieController.router, prefix="/categorie", tags=["categorie"])
-app.include_router(categorieVideoController.router, prefix="/categorie_video", tags=["categorie_video"])
+appli.include_router(enfantController.router, prefix="/enfant", tags=["enfant"])
+appli.include_router(parentController.router, prefix="/parent", tags=["parent"])
+appli.include_router(adminController.router, prefix="/admin", tags=["admin"])
+appli.include_router(tempsEcranController.router, prefix="/tempsEcran", tags=["tempsEcran"])
+appli.include_router(videoController.router, prefix="/video", tags=["video"])
+appli.include_router(saisonController.router, prefix="/saison", tags=["saison"])
+appli.include_router(serieController.router, prefix="/serie", tags=["serie"])
+appli.include_router(categorieController.router, prefix="/categorie", tags=["categorie"])
+appli.include_router(categorieVideoController.router, prefix="/categorie_video", tags=["categorie_video"])
 # app.include_router(auth.router, prefix="/categorie", tags=["categorie"])
 
 @app.get("/")
@@ -38,9 +38,9 @@ def read_root():
     return {"Hello": "World"}
 
 
-app.mount("/videos", StaticFiles(directory="media/videos"), name="videos")
+appli.mount("/videos", StaticFiles(directory="media/videos"), name="videos")
 
-app.mount("/images", StaticFiles(directory="media/couvertures"), name="images")
+appli.mount("/images", StaticFiles(directory="media/couvertures"), name="images")
 
 
 # @app.get("/media")
